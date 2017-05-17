ce76f45add13bf097a0d4e008fdf97edd6f7cd31
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-05-17 15:18:53 +0700

    initial. xmodem crc-16 calculation added

---
+++ source/crc.d
@@ -0,0 +1,36 @@
+module xymodem.crc;
+
+/**
+ * ZMODEM CRC (CRC-16/ACORN, CRC-16/LTE) calculation.
+ *
+ * Params:
+ *		crc =	current CRC value. Result of each calculation will be plased to it.
+ * 		buff =	bytes for calculation
+ */
+void crc16(ref ushort crc, in ubyte[] buff) pure nothrow @safe @nogc
+{
+	immutable ushort poly = 0x1021; // XMODEM CRC polynomic
+
+	foreach(const ubyte b; buff)
+	{
+		crc ^= (cast(ushort) b) << 8;
+
+		for(size_t i = 0; i < 8; i++)
+		{
+			bool bit15set = (crc & 0x8000) != 0;
+
+			crc <<= 1;
+
+			if(bit15set)
+				crc ^= poly;
+		}
+	}
+}
+
+unittest
+{
+	ushort crc;
+	crc16(crc, [1, 2, 3, 4, 5]);
+
+	assert(crc == 0x8208);
+}
