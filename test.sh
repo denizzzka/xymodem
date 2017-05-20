0ca006ed1b364554b3dc4a423d13146039f2f097
Denis Feklushkin <denis.feklushkin@gmail.com>
2017-05-20 16:48:06 +0700

    CI test script works

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ev
+set -e
 
 socat -x \
 EXEC:'rb --ymodem' \
@@ -10,3 +10,5 @@ EXEC:'./xymodem_integration_test --filename=integration_test/2KiB_random.bin'
 #~ socat -x \
 #~ EXEC:'rb --ymodem' \
 #~ EXEC:'sb --1k --ymodem integration_test/bbcsched.txt'
+
+diff test_result_file.bin integration_test/2KiB_random.bin
